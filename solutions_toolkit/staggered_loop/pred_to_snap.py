dc253551ea289ec1b7db85d7da8a48464d0ebd28
Scott <scott.levin@indico.io>
2021-04-13 09:30:21 -0400

    updated readme

--- solutions_toolkit/staggered_loop/pred_to_snap.py
+++ solutions_toolkit/staggered_loop/pred_to_snap.py
@@ -4,15 +4,18 @@ from solutions_toolkit.indico_wrapper import Workflow
 
 KEYS_TO_REMOVE_FROM_PREDICTION = ["confidence", "text"]
 
-# TODO: incorporate Workflow class -> i.e. get reviewed predictions for workflow/ submission IDs, OCR text, 
-# and (optional) filename 
+# TODO: incorporate Workflow class -> i.e. get reviewed predictions for workflow/ submission IDs, OCR text,
+# and (optional) filename
 # TODO: add method to write to snapshot-like CSV
 
+
 class StaggeredLoop:
-    
+
     _keys_to_remove_from_prediction = KEYS_TO_REMOVE_FROM_PREDICTION
 
-    def __init__(self, workflow_id: int, submission_ids: List[int] = [], model_name: str = ""):
+    def __init__(
+        self, workflow_id: int, submission_ids: List[int] = [], model_name: str = ""
+    ):
         self.workflow_id = workflow_id
         self.submission_ids = submission_ids
         self.model_name = model_name
@@ -32,35 +35,42 @@ class StaggeredLoop:
                 return True
         return False
 
-    def get_workflow_predictions(self, workflow_api: Workflow):
-        submissions = workflow_api.get_complete_submission_objects(self.workflow_id, self.submission_ids)
+    def get_workflow_predictions(self, workflow_api: Workflow) -> None:
+        submissions = workflow_api.get_complete_submission_objects(
+            self.workflow_id, self.submission_ids
+        )
         wf_results = workflow_api._get_submission_results(submissions)
-        predictions = [self._get_reviewed_predictions(result) for result in wf_results]
-        texts = [workflow_api.get_ondoc_ocr_from_etl_url(wf["etl_output"]).full_text for wf in wf_results]
-        filenames = [sub.input_filename for sub in submissions]
-        for preds, text, filename in zip(predictions, texts, filenames):
-            yield WorkflowResult(preds, text, filename)
+        for i in range(len(wf_results)):
+            predictions = self._get_nested_predictions(wf_results[i])
+            text = workflow_api.get_ondoc_ocr_from_etl_url(wf_results[i]["etl_output"]).full_text
+            filename = submissions[i].input_filename
+            self.workflow_results.append(WorkflowResult(predictions, text, filename))
 
-    def _get_reviewed_predictions(self, wf_result: dict):
+
+    def _get_nested_predictions(self, wf_result: dict) -> List[dict]:
         results = wf_result["results"]["document"]["results"]
-        model_names = list(results.keys())
+        available_model_names = list(results.keys())
         if self.model_name:
-            self._check_is_valid_model_name(model_names)
-        elif len(model_names) > 1:
-            raise Exception(f"Multiple models available, you must set self.model_name to one of {model_names}")
+            self._check_is_valid_model_name(available_model_names)
+        elif len(available_model_names) > 1:
+            raise Exception(
+                f"Multiple models available, you must set self.model_name to one of {available_model_names}"
+            )
         else:
-            self.model_name = model_names[0]
+            self.model_name = available_model_names[0]
         return results[self.model_name]["final"]
 
     def _check_is_valid_model_name(self, model_names: List[str]) -> bool:
         if self.model_name not in model_names:
-            raise Exception(f"{self.model_name} is an available model name. Options: {model_names}")
+            raise Exception(
+                f"{self.model_name} is an available model name. Options: {model_names}"
+            )
 
     def _remove_unneeded_keys(self, prediction: dict):
         for key_to_remove in self._keys_to_remove_from_prediction:
             prediction.pop(key_to_remove, None)
 
-    
+
 @dataclass
 class WorkflowResult:
     predictions: List[dict]
