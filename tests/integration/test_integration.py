4495c8e291a8a8e14195c3a58968a991ce9504f2
jonas <jonas.kazlauskas@me.com>
2021-04-19 10:06:07 -0700

    Update tests filestructure and add integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,4 +1,5 @@
 from solutions_toolkit.auto_review import ReviewConfiguration, Reviewer
+from solutions_toolkit.row_association import Association
 from tests.conftest import MODEL_NAME
 
 
@@ -40,7 +41,10 @@ def test_submit_auto_review(workflow_wrapper, function_submission_ids):
             assert pred["accepted"] == True
 
 
-def test_workflow_submit_wait_get_results(workflow_wrapper, workflow_id, pdf_filepath):
+def test_workflow_submit_and_get_rows(workflow_wrapper, workflow_id, pdf_filepath):
+    """
+    Submit a document to workflow, get results and ocr object, then association line items
+    """
     sub_ids = workflow_wrapper.submit_documents_to_workflow(
         workflow_id=workflow_id, pdf_filepaths=[pdf_filepath]
     )
@@ -49,5 +53,10 @@ def test_workflow_submit_wait_get_results(workflow_wrapper, workflow_id, pdf_fil
     predictions = sub_result["results"]["document"]["results"][MODEL_NAME][
         "pre_review"
     ]
-    assert isinstance(predictions, list)
-    assert isinstance(predictions[0], dict)
+    litems = Association(["Previous Position", "Previous Organization"], predictions=predictions)
+    ondoc_ocr = workflow_wrapper.get_ondoc_ocr_from_etl_url(sub_result["etl_output"])
+    litems.get_bounding_boxes(ocr_tokens=ondoc_ocr.token_objects)
+    litems.assign_row_number()
+    for pred in litems.updated_predictions:
+        if pred["label"] in ["Previous Position", "Previous Organization"]:
+            assert "row_number" in pred
