3726d2bbf3273e4b10362960e9724e781aa82f64
annaliu-indico <anna.liu@indico.io>
2021-05-04 17:18:19 -0400

    adding additional standard object tests

--- tests/ocr/test_standard_ocr.py
+++
@@ -1,7 +0,0 @@
-from solutions_toolkit.ocr import Standard
-
-
-def test_standard_ocr_object(standard_ocr_object):
-    standard_ocr = Standard(standard_ocr_object)
-    assert isinstance(standard_ocr, Standard)
-    assert standard_ocr.total_pages == 8
