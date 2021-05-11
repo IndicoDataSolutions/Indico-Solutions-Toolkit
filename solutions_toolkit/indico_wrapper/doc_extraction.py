9c24da5004bfc0cece9593a3e8e424f7b3566adc
annaliu-indico <anna.liu@indico.io>
2021-05-11 17:59:58 -0400

    editing and renaming doc extraction class and tests

--- solutions_toolkit/indico_wrapper/doc_extraction.py
+++ solutions_toolkit/indico_wrapper/doc_extraction.py
@@ -2,6 +2,7 @@ from typing import List
 from indico import IndicoClient
 from indico.queries import (
     DocumentExtraction,
+    Job,
     JobStatus
 )
 from solutions_toolkit.indico_wrapper import IndicoWrapper
@@ -14,83 +15,51 @@ class DocExtraction(IndicoWrapper):
     Class to support DocumentExtraction-related API calls
     """
 
-    def __init__(self, client: IndicoClient):
+    def __init__(self, client: IndicoClient, preset_config: None):
         self.client = client
+        self.preset_config = preset_config
 
-    def get_extraction_jobs(self, preset_config: str, pdf_filepaths: List) -> List:
+    def _submit_to_ocr(self, preset_config: str, pdf_filepaths: List[str]) -> List[Job]:
         """
         Args:
             preset_config (str): Preset configuration setting
             pdf_filepaths (List[str]): List of paths to local documents you would like to submit for extraction
 
         Returns:
-            jobs (List): List of job ids
+            jobs (List[Job]): List of job ids
         """
-        jobs = self.client.call(
+        return self.client.call(
             DocumentExtraction(files=pdf_filepaths, json_config={"preset_config": preset_config}))
-        return jobs
 
-    def get_extracted_data(self, preset_config: str, pdf_filepaths: List):
+    def run_ocr(self, preset_config: str, pdf_filepaths: List[str]):
         """
         Args:
             preset_config (str): Preset configuration setting
-            pdf_filepaths (List): List of paths to local documents you would like to submit for extraction
+            pdf_filepaths (List[str]): List of paths to local documents you would like to submit for extraction
 
         Returns:
-            extracted_data: data from DocumentExtraction
+            extracted_data (List[OcrObjects]): data from DocumentExtraction converted to OCR objects if applicable
         """
-        jobs = self.get_extraction_jobs(preset_config, pdf_filepaths)
-        if len(jobs) == 1:
-            job = self.client.call(JobStatus(id=jobs[0].id, wait=True))
-            extracted_data = self.get_storage_object(job.result)
-            return extracted_data
-        elif len(jobs) > 1:
-            extracted_data = {}
-            for ind, job in enumerate(jobs):
-                file = pdf_filepaths[ind]
-                ocr = self.client.call(JobStatus(id=job.id, wait=True))
-                result = self.get_storage_object(ocr.result)
-                extracted_data[file] = result
-            return extracted_data
+        jobs = self._submit_to_ocr(preset_config, pdf_filepaths)
+        extracted_data = []
+        for job in jobs:
+            ocr = self.client.call(JobStatus(id=job.id, wait=True))
+            result = self.get_storage_object(ocr.result)
+            extracted_data.append(self.convert_ocr_objects(preset_config=preset_config, data=result))
+        return extracted_data
 
-    def get_ocr_objects(self, preset_config: str, pdf_filepaths: List):
-        results = self.get_extracted_data(preset_config, pdf_filepaths)
-        if len(pdf_filepaths) == 1:
-            if preset_config == "ondocument":
-                results = OnDoc(results)
-            elif preset_config == "standard":
-                results = StandardOcr(results)
-        elif len(pdf_filepaths) > 1:
-            for result in results:
-                if preset_config == "ondocument":
-                    results[result] = OnDoc(results[result])
-                elif preset_config == "standard":
-                    results[result] = StandardOcr(results[result])
-        return results
+    def convert_ocr_objects(self, preset_config: str, data):
+        """
+        Args:
+            preset_config (str): Preset configuration setting
+            data: data from DocumentExtraction
 
-    def get_doc_full_text(self, preset_config: str, pdf_filepaths: List):
-        results = self.get_ocr_objects(preset_config, pdf_filepaths)
-        if len(pdf_filepaths) == 1:
-            results = results.full_text
-        elif len(pdf_filepaths) > 1:
-            for result in results:
-                results[result] = results[result].full_text
-        return results
-
-    def get_doc_page_texts(self, preset_config: str, pdf_filepaths: List):
-        results = self.get_ocr_objects(preset_config, pdf_filepaths)
-        if len(pdf_filepaths) == 1:
-            results = results.page_texts
-        elif len(pdf_filepaths) > 1:
-            for result in results:
-                results[result] = results[result].page_texts
-        return results
-
-    def get_doc_page_results(self, preset_config: str, pdf_filepaths: List):
-        results = self.get_ocr_objects(preset_config, pdf_filepaths)
-        if len(pdf_filepaths) == 1:
-            results = results.page_results
-        elif len(pdf_filepaths) > 1:
-            for result in results:
-                results[result] = results[result].page_results
-        return results
+        Returns:
+            data (OcrObject): data converted to an ocr object
+        """
+        if preset_config == "ondocument":
+            return OnDoc(data)
+        elif preset_config == "standard":
+            return StandardOcr(data)
+        else:
+            return data
