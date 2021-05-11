520ac63ab6c098df532ba199b8282b085b4e1a29
Scott <scott.levin@indico.io>
2021-05-11 14:06:13 -0400

    updating example

--- examples/submitting_to_workflow.py
+++ examples/submitting_to_workflow.py
@@ -5,17 +5,18 @@ from solutions_toolkit import create_client
 WORKFLOW_ID = 1418
 HOST = "app.indico.io"
 API_TOKEN_PATH = "./indico_api_token.txt"
-BATCH_TO_SUBMIT = ["path/to/doc.pdf", "path/to/doc.pdf"]
 
-# EXAMPLE 1 -> Submit a batch of documents and retrieve results
-
+# Instantiate the workflow class
 client = create_client(HOST, API_TOKEN_PATH)
-
 wflow = Workflow(client)
 
-submission_ids = wflow.submit_documents_to_workflow(WORKFLOW_ID, BATCH_TO_SUBMIT)
+# Collect files to submit
+fp = FileProcessing()
+fp.get_file_paths_from_dir("./datasets/disclosures/")
 
-submission_results = wflow.get_submission_results_from_ids(submission_ids, timeout=60)
-
-for result in submission_results:
-    print(result.predictions)
+# Submit documents, await the results and write the results to CSV in batches of 10
+for paths in fp.batch_files(batch_size=10):
+    submission_ids = wflow.submit_documents_to_workflow(WORKFLOW_ID, paths)
+    submission_results = wflow.get_submission_results_from_ids(submission_ids)
+    for filename, result in zip(paths, submission_results):
+        result.predictions.to_csv("./results.csv", filename=filename, append_if_exists=True)
