90ef6c729cd4eade4cfd02c5cafa0b101f417325
annaliu-indico <anna.liu@indico.io>
2021-05-11 15:56:34 -0400

    editing doc extraction and tests for indico client

--- tests/indico_wrapper/test_doc_extraction.py
+++ tests/indico_wrapper/test_doc_extraction.py
@@ -1,18 +1,5 @@
-from indico import IndicoClient
-
-from tests.conftest import API_TOKEN_PATH, HOST_URL, API_TOKEN
-from solutions_toolkit.indico_wrapper import DocExtraction
 from solutions_toolkit.ocr import OnDoc
 from solutions_toolkit.ocr import StandardOcr
-
-
-def test_doc_extraction_init():
-    doc_extraction = DocExtraction(
-        HOST_URL,
-        api_token_path=API_TOKEN_PATH,
-        api_token=API_TOKEN,
-    )
-    assert isinstance(doc_extraction.indico_client, IndicoClient)
 
 
 def test_get_extraction_jobs(doc_extraction_wrapper, pdf_filepath):
