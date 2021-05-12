365da20986b2dac780f302e2f6e809ec5edfdf01
Scott <scott.levin@indico.io>
2021-05-12 15:35:16 -0400

    refactored row association module

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -21,6 +21,6 @@ def test_workflow_submit_and_get_rows(indico_client, workflow_id, pdf_filepath):
     ondoc_ocr = wflow.get_ondoc_ocr_from_etl_url(sub_result.etl_url)
     litems.get_bounding_boxes(ocr_tokens=ondoc_ocr.token_objects)
     litems.assign_row_number()
-    for pred in litems.updated_predictions:
+    for pred in litems.updated_predictions.tolist():
         if pred["label"] in ["Previous Position", "Previous Organization"]:
             assert "row_number" in pred
