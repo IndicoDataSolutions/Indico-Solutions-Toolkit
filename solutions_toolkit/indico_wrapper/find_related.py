9e1e257b162782d5832d8e10172cff22a7e7efa5
Scott <scott.levin@indico.io>
2021-05-13 12:14:59 -0400

    refactored to simplify

--- solutions_toolkit/indico_wrapper/find_related.py
+++ solutions_toolkit/indico_wrapper/find_related.py
@@ -1,4 +1,4 @@
-from indico.queries import ListWorkflows
+from typing import List
 from indico import IndicoClient
 from solutions_toolkit.indico_wrapper import IndicoWrapper
 
@@ -33,9 +33,9 @@ class FindRelated(IndicoWrapper):
             }
         """
         res = self.graphQL_request(query, {"id": dataset_id})["dataset"]
-        wf_list = self.client.call(ListWorkflows(dataset_ids=[dataset_id]))
+        wf_list = self._get_workflow_data(dataset_id=dataset_id)
         return {
-            "workflow_ids": [wf.id for wf in wf_list],
+            "workflow_ids": [wf["id"] for wf in wf_list],
             "model_groups": res["modelGroups"],
             "dataset_name": res["name"],
             "dataset_id": dataset_id,
@@ -59,18 +59,14 @@ class FindRelated(IndicoWrapper):
         res = self.graphQL_request(query, {"id": model_group_id})["modelGroups"][
             "modelGroups"
         ]
-        dataset_workflows = self._get_workflow_data(dataset_id=res[0]["datasetId"])
+        workflow_response = self._get_workflow_data(dataset_id=res[0]["datasetId"])
+        workflow_id = self._match_workflow_and_model_group(workflow_response, model_group_id)
         return {
             "model_group_name": res[0]["name"],
             "model_group_id": model_group_id,
             "selected_model_id": res[0]["selectedModel"]["id"],
             "dataset_id": res[0]["datasetId"],
-            "workflow_ids": [
-                wf["id"]
-                for wf in dataset_workflows
-                for model in wf["reviewableModelGroups"]
-                if model["id"] == model_group_id
-            ],
+            "workflow_id": workflow_id["id"]
         }
 
     def model_id(self, model_id: int):
@@ -91,33 +87,20 @@ class FindRelated(IndicoWrapper):
             }
         """
         model_groups = self.graphQL_request(query)["modelGroups"]["modelGroups"]
-        relevant_group = None
-        for group in model_groups:
-            for model in group["models"]:
-                if model["id"] == model_id:
-                    relevant_group = group
-                    break
-        if not relevant_group:
-            raise Exception(f"ID {model_id} doesn't exist or you don't have access to it")
+        relevant_group = self._match_model_group_and_model_id(model_groups, model_id)
         workflow_data = self._get_workflow_data(dataset_id=[relevant_group["datasetId"]])
-        workflow_id = None
-        for flow in workflow_data:
-            for group in flow["reviewableModelGroups"]:
-                if group["id"] == relevant_group["id"]:
-                    workflow_id = flow["id"]
-                    break
-        workflow_id = [i["id"] for i in workflow_data if i["reviewableModelGroups"]]
+        workflow_id = self._match_workflow_and_model_group(workflow_data, relevant_group["id"])
         return {
             "model_id": model_id,
             "model_group_id": relevant_group["id"],
             "model_name": relevant_group["name"],
             "dataset_id": relevant_group["datasetId"],
             "selected_model_id": relevant_group["selectedModel"]["id"],
-            "workflow_id": workflow_id
+            "workflow_id": workflow_id["id"]
         }
 
 
-    def _get_workflow_data(self, dataset_id: int = None, workflow_id: int = None):
+    def _get_workflow_data(self, dataset_id: int = None, workflow_id: int = None) -> List[dict]:
         query = """
             query ListWorkflows($datasetIds: [Int], $workflowIds: [Int]) {
                 workflows(datasetIds: $datasetIds, workflowIds: $workflowIds) {
@@ -143,3 +126,19 @@ class FindRelated(IndicoWrapper):
             variables = {"workflowIds": [workflow_id]}
         res = self.graphQL_request(query, variables)
         return res["workflows"]["workflows"]
+
+    @staticmethod
+    def _match_workflow_and_model_group(workflow_response: List[dict], model_group:int) -> dict:
+        for workflow in workflow_response:
+            for group in workflow["reviewableModelGroups"]:
+                if group["id"] == model_group:
+                    return workflow
+        raise Exception(f"Model Group {model_group} doesn't exist in {workflow_response}")
+
+    @staticmethod
+    def _match_model_group_and_model_id(model_response: List[dict], model_id:int) -> dict:
+        for group in model_response:
+            for model in group["models"]:
+                if model["id"] == model_id:
+                    return group
+        raise Exception(f"Model ID {model_id} doesn't exist or you don't have access to it")
--- tests/indico_wrapper/test_find_related.py
+++ tests/indico_wrapper/test_find_related.py
@@ -12,15 +12,16 @@ def test_dataset_and_model_group_id(indico_client, dataset_obj):
     assert isinstance(dataset_related["workflow_ids"][0], int)
     assert isinstance(dataset_related["model_groups"][0]["selectedModel"]["id"], int)
     model_related = finder.model_group_id(dataset_related["model_groups"][0]["id"])
-    assert isinstance(model_related["workflow_ids"][0], int)
+    assert isinstance(model_related["workflow_id"], int)
 
 def test_model_id(indico_client, dataset_obj):
     finder = FindRelated(indico_client)
     dataset_id = dataset_obj.id
     dataset_related = finder.dataset_id(dataset_id)
     model_id = dataset_related["model_groups"][0]["selectedModel"]["id"]
+    model_group_id = dataset_related["model_groups"][0]["id"]
     model_data = finder.model_id(model_id)
     assert model_data["model_id"] == model_id
     assert model_data["dataset_id"] == dataset_id
-    assert isinstance(model_data["workflow_id"][0], int)
-    assert isinstance(model_data["model_group_id"], int)
+    assert model_data["model_group_id"] == model_group_id
+    assert isinstance(model_data["workflow_id"], int)
