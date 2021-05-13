6c587479face847d3f86e1659da2e0d5ca910ba9
annaliu-indico <anna.liu@indico.io>
2021-05-13 13:44:10 -0400

    editing custom ocr object properties

--- solutions_toolkit/ocr/customocr_object.py
+++ solutions_toolkit/ocr/customocr_object.py
@@ -18,19 +18,22 @@ class CustomOcr:
         """
         Return full document text as string
         """
-        if "text" in self.customocr:
+        if self.customocr is dict and "text" in self.customocr:
             return self.customocr["text"]
-        else:
-            raise Exception(f"JSON configuration setting does not have full text.")
+        elif self.customocr is List[dict] and "pages" in self.customocr[0]:
+            if "text" in self.customocr[0]["pages"][0]:
+                return "\n".join(page["pages"][0]["text"] for page in self.customocr)
+        raise Exception(f"JSON configuration setting does not have full text.")
 
     @property
     def page_texts(self) -> List[str]:
         """
         Return list of page-level text
         """
-        if "pages" in self.customocr:
-            return [page["text"] for page in self.customocr["pages"]]
-        else:
-            raise Exception(
-                f"JSON configuration setting does not have page-level text."
-            )
+        if self.customocr is dict and "pages" in self.customocr:
+            if "text" in self.customocr["pages"]:
+                return [page["text"] for page in self.customocr["pages"]]
+        elif self.customocr is List[dict] and "pages" in self.customocr[0]:
+            if "text" in self.customocr[0]["pages"][0]:
+                return [page["pages"][0]["text"] for page in self.customocr]
+        raise Exception(f"JSON configuration setting does not have page-level text.")
