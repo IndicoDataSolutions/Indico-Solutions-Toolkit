f4ad41b3094a970d9d149af4b4ff8759199747da
jonas <jonas.kazlauskas@me.com>
2021-05-14 11:40:51 -0700

    Add find related example

---
+++ examples/find_related_ids.py
@@ -0,0 +1,21 @@
+from solutions_toolkit.indico_wrapper import FindRelated
+from solutions_toolkit import create_client
+
+MODEL_ID = 33318
+HOST = "app.indico.io"
+API_TOKEN_PATH = "./solutions_test_token.txt"   #./indico_api_token.txt"
+
+# Instantiate the FindRelated class
+client = create_client(HOST, API_TOKEN_PATH)
+finder = FindRelated(client)
+
+# Find related 
+related = finder.model_id(MODEL_ID)
+
+print(related["workflow_id"])
+print(related["dataset_id"])
+print(related["selected_model_id"])
+print(related["model_group_id"])
+print(related["model_name"])
+print(related["model_id"])
+
