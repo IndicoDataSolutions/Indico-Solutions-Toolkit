eab8588ea42a9435ed78758754a4573553f7f01e
Scott <scott.levin@indico.io>
2021-07-27 11:46:50 -0400

    accounting for alternate classification formatting

--- indico_toolkit/types/__init__.py
+++ indico_toolkit/types/__init__.py
@@ -2,4 +2,4 @@ from .workflow_object import WorkflowResult
 from .predictions import Predictions
 from .teach_task import TeachTask
 from .extractions import Extractions
-from .classification import Classification
+from .classification import Classification, ClassificationMGP
