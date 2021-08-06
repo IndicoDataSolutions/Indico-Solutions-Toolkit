2a6477e9bfbe08204367c4fdae2ed91e0c701e58
Scott <scott.levin@indico.io>
2021-08-06 13:07:40 -0400

    added stagger dataset upload and refactor

--- indico_toolkit/indico_wrapper/dataset.py
+++ indico_toolkit/indico_wrapper/dataset.py
@@ -1,6 +1,9 @@
 from typing import List
 from functools import wraps
 import pandas as pd
+import math
+import time
+from datetime import datetime
 from indico import IndicoClient
 from indico.types import Dataset
 from indico.queries import (
@@ -11,44 +14,76 @@ from indico.queries import (
     AddFiles,
     ProcessFiles,
     DeleteDataset,
+    CreateEmptyDataset,
 )
+
 from indico_toolkit.indico_wrapper import IndicoWrapper
-
-
-def dataset_id_required(fn):
-    """
-    'Datasets' optionally requires an ID on instantiation. For methods that require that it exists, 
-    throw a runtimeError if 'dataset_id' not set.
-    """
-
-    @wraps(fn)
-    def check_dataset_id(self, *args, **kwargs):
-        if not isinstance(self.dataset_id, int):
-            raise RuntimeError(
-                "You must set the 'dataset_id' attribute to use this method"
-            )
-        return fn(self, *args, **kwargs)
-
-    return check_dataset_id
+from indico_toolkit.pipelines import FileProcessing
 
 
 class Datasets(IndicoWrapper):
-    def __init__(self, client: IndicoClient, dataset_id: int = None):
+    def __init__(self, client: IndicoClient):
         self.client = client
-        self.dataset_id = dataset_id
 
-    @dataset_id_required
-    def get_dataset(self):
-        return self.client.call(GetDataset(self.dataset_id))
+    def get_dataset(self, dataset_id: int):
+        return self.client.call(GetDataset(dataset_id))
 
-    @dataset_id_required
-    def download_export(self, **kwargs) -> pd.DataFrame:
-        export_id = self._create_export(**kwargs)
+    def download_export(self, dataset_id: int, **kwargs) -> pd.DataFrame:
+        export_id = self._create_export(dataset_id, **kwargs)
         return self.client.call(DownloadExport(export_id))
 
-    def add_files_to_dataset(self, filepaths: List[str]) -> Dataset:
-        dataset = self._upload_files(filepaths)
-        return self._process_uploaded_files(dataset)
+    def add_files_to_dataset(self, dataset_id: int, filepaths: List[str]) -> Dataset:
+        """
+        Upload documents to an existing dataset and wait for them to OCR
+        """
+        dataset = self.client.call(
+            AddFiles(dataset_id=dataset_id, files=filepaths, wait=True)
+        )
+        datafile_ids = [f.id for f in dataset.files if f.status == "DOWNLOADED"]
+        return self.client.call(
+            ProcessFiles(dataset_id=dataset_id, datafile_ids=datafile_ids, wait=True)
+        )
+
+    def create_empty_dataset(
+        self, dataset_name: str, dataset_type: str = "DOCUMENT"
+    ) -> Dataset:
+        """
+        Create an empty dataset
+        Args:
+            name (str): Name of the dataset
+            dataset_type (str, optional): TEXT, IMAGE, or DOCUMENT. Defaults to "DOCUMENT".
+        """
+        return self.client.call(CreateEmptyDataset(dataset_name, dataset_type))
+
+    def create_large_doc_dataset(
+        self,
+        dataset_name: str,
+        filepaths: List[str],
+        batch_size: int = 3,
+        post_batch_delay_secs: int = 1,
+        verbose: bool = True,
+    ) -> Dataset:
+        """[summary]
+
+        Args:
+            dataset_name (str): create a name for the dataset 
+            filepaths (List[str]): files you want to upload
+            batch_size (int, optional): number of files to process at a time. Defaults to 3.
+            post_batch_delay_secs (int, optional): number of seconds to pause between batches. Defaults to 1.
+            verbose (bool, optional): print updates as each batch processes. Defaults to True.
+        """
+        fp = FileProcessing(filepaths)
+        dataset_id = self.create_empty_dataset(dataset_name).id
+        number_of_batches = int(math.ceil(len(filepaths) / batch_size))
+        for batch_fpaths in fp.batch_files(batch_size):
+            dataset = self.add_files_to_dataset(dataset_id, batch_fpaths)
+            if verbose:
+                number_of_batches -= 1
+                print(
+                    f"Finished batch at {datetime.now().time()}: {number_of_batches} remaining"
+                )
+            time.sleep(post_batch_delay_secs)
+        return dataset
 
     def create_dataset(self, filepaths: List[str], dataset_name: str) -> Dataset:
         dataset = self.client.call(CreateDataset(name=dataset_name, files=filepaths,))
@@ -61,8 +96,7 @@ class Datasets(IndicoWrapper):
         """
         return self.client.call(DeleteDataset(id=dataset_id))
 
-    @dataset_id_required
-    def get_dataset_metadata(self) -> List[dict]:
+    def get_dataset_metadata(self, dataset_id: int) -> List[dict]:
         """
         Get list of dataset files with information like file name, status, and number of pages
         """
@@ -81,27 +115,18 @@ class Datasets(IndicoWrapper):
             }
         """
         dataset = self.graphQL_request(
-            graphql_query=query, variables={"id": self.dataset_id}
+            graphql_query=query, variables={"id": dataset_id}
         )
         return dataset["dataset"]["files"]
-    
-    @dataset_id_required
-    def get_col_name_by_id(self, col_id: int) -> str:
-        dataset = self.get_dataset()
+
+    def get_col_name_by_id(self, dataset_id: int, col_id: int) -> str:
+        dataset = self.get_dataset(dataset_id)
         return next(c.name for c in dataset.datacolumns if c.id == col_id)
 
-    def _create_export(self, **kwargs) -> int:
+    def _create_export(self, dataset_id: int, **kwargs) -> int:
         """
         Returns export ID
         """
-        export = self.client.call(CreateExport(dataset_id=self.dataset_id, **kwargs))
+        export = self.client.call(CreateExport(dataset_id=dataset_id, **kwargs))
         return export.id
 
-    def _process_uploaded_files(self, dataset: Dataset) -> Dataset:
-        datafile_ids = [f.id for f in dataset.files if f.status == "DOWNLOADED"]
-        return self.client.call(
-            ProcessFiles(dataset_id=dataset.id, datafile_ids=datafile_ids, wait=True)
-        )
-
-    def _upload_files(self, filepaths):
-        return self.client.call(AddFiles(dataset_id=self.dataset_id, files=filepaths))
