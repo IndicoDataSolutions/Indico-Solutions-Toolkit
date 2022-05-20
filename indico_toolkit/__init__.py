cef77523d29e46cdcf8a29a81d422e35c77dedb5
Nathanael Shim <nathanaelshim@Nathanaels-MacBook-Pro.local>
2022-05-20 11:41:00 -0700

    v2.0.1

--- indico_toolkit/__init__.py
+++ indico_toolkit/__init__.py
@@ -1,21 +1,6 @@
 """A package to support Indico IPA development"""
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 from .errors import *
 from .client import create_client
 from .retry import retry
-
-from indico_toolkit.indico_wrapper import (
-    IndicoWrapper,
-    retry,
-    Workflow,
-    Datasets,
-    Reviewer,
-    DocExtraction,
-    Download
-)
-
-from indico_toolkit.pipelines import FileProcessing
-from indico_toolkit.snapshots import Snapshot
-from indico_toolkit.metrics import ExtractionMetrics, CompareModels, Plotting
-from indico_toolkit.highlighter import Highlighter
