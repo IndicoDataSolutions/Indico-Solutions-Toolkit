1758ad2420e274ae5519d1b71c6c48bfb028ca3b
andrew8bit <andrewbith@gmail.com>
2022-05-24 09:49:33 -0700

    Updated Testing

--- tests/snapshots/conftest.py
+++ tests/snapshots/conftest.py
@@ -4,12 +4,18 @@ import pytest
 @pytest.fixture(scope="session")
 def snapshot_classes():
     snapshot_classes = [
-        "Company Name",
-        "Stock Symbol",
-        "Trader's District",
-        "Trader's Name",
-        "Transaction Amount",
-        "Transaction date",
-        "Transaction type",
+        'Grand Total Due',
+        'Line Cost',
+        'Line Description',
+        'Quantity',
+        'Sub Total',
+        'Tax',
+        'Transaction Date',
+        'Transaction Time',
+        'Vendor City',
+        'Vendor Name',
+        'Vendor State',
+        'Vendor Street Address',
+        'Vendor Zip Code'
     ]
     return snapshot_classes
