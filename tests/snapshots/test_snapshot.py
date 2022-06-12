1e36fb54e27ad4e512abd540a09862c2cd9a93ad
andrew8bit <andrewbith@gmail.com>
2022-06-11 19:08:29 -0700

    Adjusted tests for lowered data sample size

--- tests/snapshots/test_snapshot.py
+++ tests/snapshots/test_snapshot.py
@@ -96,12 +96,13 @@ def test_split_and_write_to_csv(snapshot_csv_path):
     with tempfile.TemporaryDirectory() as dirpath:
         snap.split_and_write_to_csv(dirpath, num_splits=3, output_base_name="my_split")
         original = pd.read_csv(snapshot_csv_path)
+        assert original.shape[0] == 10  # / 3 = 3,3,4
         df1 = pd.read_csv(os.path.join(dirpath, "my_split_1.csv"))
         df2 = pd.read_csv(os.path.join(dirpath, "my_split_2.csv"))
         df3 = pd.read_csv(os.path.join(dirpath, "my_split_3.csv"))
-        assert df1.shape[0] == 49
-        assert df2.shape[0] == 49
-        assert df3.shape[0] == 51
+        assert df1.shape[0] == 3
+        assert df2.shape[0] == 3
+        assert df3.shape[0] == 4
         full = pd.concat([df1, df2, df3]).reset_index(drop=True)
         assert full.shape[0] == original.shape[0]
         assert set(full.columns) == set(original.columns)
@@ -150,13 +151,13 @@ def test_get_extraction_label_names(snapshot_csv_path, snapshot_classes):
 
 def test_number_of_samples(snapshot_csv_path):
     snap = Snapshot(snapshot_csv_path)
-    assert snap.number_of_samples == 149
+    assert snap.number_of_samples == 10
 
 
 def test_get_all_labeled_text(snapshot_csv_path):
     snap = Snapshot(snapshot_csv_path)
     labeled_text = snap.get_all_labeled_text("Vendor State")
-    assert len(labeled_text) == 149
+    assert len(labeled_text) == 10
     assert isinstance(labeled_text[0], str)
     assert labeled_text[0] == "WY"
 
@@ -164,7 +165,7 @@ def test_get_all_labeled_text(snapshot_csv_path):
 def test_get_all_labeled_text_per_doc(snapshot_csv_path):
     snap = Snapshot(snapshot_csv_path)
     labeled_text = snap.get_all_labeled_text("Vendor State", return_per_document=True)
-    assert len(labeled_text) == 149
+    assert len(labeled_text) == 10
     assert isinstance(labeled_text[0], list)
     assert len(labeled_text[0]) == 1
     assert labeled_text[0][0] == "WY"
