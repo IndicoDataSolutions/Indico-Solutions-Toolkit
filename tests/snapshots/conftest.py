2175525dc4053b112d43182935732fd1bc362b1a
andrew8bit <andrewbith@gmail.com>
2022-06-11 19:05:30 -0700

    Black formatting

--- tests/snapshots/conftest.py
+++ tests/snapshots/conftest.py
@@ -4,18 +4,18 @@ import pytest
 @pytest.fixture(scope="session")
 def snapshot_classes():
     snapshot_classes = [
-        'Grand Total Due',
-        'Line Cost',
-        'Line Description',
-        'Quantity',
-        'Sub Total',
-        'Tax',
-        'Transaction Date',
-        'Transaction Time',
-        'Vendor City',
-        'Vendor Name',
-        'Vendor State',
-        'Vendor Street Address',
-        'Vendor Zip Code'
+        "Grand Total Due",
+        "Line Cost",
+        "Line Description",
+        "Quantity",
+        "Sub Total",
+        "Tax",
+        "Transaction Date",
+        "Transaction Time",
+        "Vendor City",
+        "Vendor Name",
+        "Vendor State",
+        "Vendor Street Address",
+        "Vendor Zip Code",
     ]
     return snapshot_classes
