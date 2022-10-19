55a5103e66004f890ffa449227aa7f5bee594702
andrew8bit <andrewbith@gmail.com>
2022-10-19 10:57:40 -0700

    updated auto_review example to include custom functions

--- examples/auto_review_predictions.py
+++ examples/auto_review_predictions.py
@@ -1,13 +1,13 @@
 """
 Submit documents to a workflow, auto review them and submit them for human review
 """
-from indico_toolkit import auto_review
 from indico_toolkit.auto_review import (
     ReviewConfiguration,
     AutoReviewer,
 )
 from indico_toolkit.indico_wrapper import Workflow
 from indico_toolkit import create_client
+from typing import List
 
 
 WORKFLOW_ID = 1234
@@ -26,6 +26,26 @@ submission_ids = wflow.submit_documents_to_workflow(
 wf_results = wflow.get_submission_results_from_ids(submission_ids)
 predictions = wf_results[0].predictions.to_list()
 
+# Make custom functions if needed
+def reject_first_n_instances(predictions:List[dict], n_instances:int, labels:List[str])->List[dict]:
+    """
+        Custom autoreview function that rejects the first n predictions for a group of labels.
+        Args:
+            predictions(List[dict]): List of all predictions
+            n_instances(int): Reject any predictions of a specific label before this number
+            labels(List[str]): List of labels to apply this logic to
+        Returns:
+            An updated list of predictions with rejected logic added to preds that match function condition
+    """
+    for label in labels:
+        matching_preds = [pred for pred in predictions if pred['label'] == label and "rejected" not in pred]
+        for i, pred in enumerate(matching_preds):
+            target_prediction = predictions[predictions.index(pred)]
+            if i < n_instances:
+                target_prediction["rejected"] = True
+    return predictions
+                
+
 # Set up reviewer and review predictions
 review_config = ReviewConfiguration(
     field_config=[
@@ -34,7 +54,14 @@ review_config = ReviewConfiguration(
             "function": "accept_by_confidence",
             "kwargs": {"conf_threshold": 0.98, "labels": ["Name", "Amount"]},
         },
-    ]
+        {
+            "function": "reject_after_n_instances",
+            "kwargs": {"n_instances": 1, "labels": ["Name", "Amount"]},
+        },
+    ],
+    custom_functions={
+        "reject_after_n_instances": reject_first_n_instances
+    }
 )
 auto_reviewer = AutoReviewer(predictions, review_config)
 auto_reviewer.apply_reviews()
