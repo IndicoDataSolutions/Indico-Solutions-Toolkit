ae46ee735039b60d4bc95b999ff6f0b2467274d7
andrew8bit <andrewbith@gmail.com>
2023-02-06 18:35:45 -0800

    Updating types

--- indico_toolkit/structure/create_structure.py
+++ indico_toolkit/structure/create_structure.py
@@ -15,8 +15,8 @@ from indico.queries import (
 )
 from indico.types import (
     OcrEngine,
-    OmnipageOcrOptionsInput,
-    ReadApiOcrOptionsInput,
+    Dataset,
+    Workflow,
     TableReadOrder,
 )
 from indico.types import Workflow
@@ -53,7 +53,7 @@ class Structure:
         files_to_upload: List[str],
         read_api: bool = True,
         **kwargs,
-    ):
+    ) -> Dataset:
         """
         Creates a dataset from a list of files.
 
@@ -107,34 +107,27 @@ class Structure:
                 omnipage_settings.update({arg: kwargs[arg]})
 
         ocr_engine = OcrEngine.READAPI if read_api else OcrEngine.OMNIPAGE
-
         if read_api:
-            ocr_settings: ReadApiOcrOptionsInput = read_api_settings
-            self.dataset = self.client.call(
-                CreateDataset(
-                    dataset_name,
-                    files=files_to_upload,
-                    dataset_type="DOCUMENT",
-                    ocr_engine=ocr_engine,
-                    read_api_ocr_options=ocr_settings,
-                )
-            )
+            ocr_settings = {"read_api_ocr_options": read_api_settings}
         else:
             omnipage_settings["table_read_order"] = (
                 TableReadOrder.ROW
                 if omnipage_settings["table_read_order"] == "row"
                 else TableReadOrder.COLUMN
             )
-            ocr_settings: OmnipageOcrOptionsInput = omnipage_settings
-            self.dataset = self.client.call(
-                CreateDataset(
-                    dataset_name,
-                    files=files_to_upload,
-                    dataset_type="DOCUMENT",
-                    ocr_engine=ocr_engine,
-                    omnipage_ocr_options=ocr_settings,
-                )
+            ocr_settings = {"omnipage_ocr_options": omnipage_settings}
+
+        self.dataset = self.client.call(
+            CreateDataset(
+                dataset_name,
+                files=files_to_upload,
+                dataset_type="DOCUMENT",
+                ocr_engine=ocr_engine,
+                **ocr_settings,
             )
+        )
+        print(f"dataset created with id: {self.dataset.id}")
+        return self.dataset
 
     def create_duplicate_dataset(
         self,
@@ -143,7 +136,7 @@ class Structure:
         times_to_copy_files=55,
         read_api=True,
         **kwargs,
-    ):
+    ) -> Dataset:
         """
         Creates a dataset w/ duplicate instances of 1 file, historically used to create a spoofed demo.
 
@@ -180,13 +173,26 @@ class Structure:
         finally:
             tempdir.cleanup()
 
-    def create_workflow(self, name: str, dataset_id: int = None):
+    def create_workflow(self, name: str, dataset_id: int = None) -> Workflow:
+        """
+
+        Args:
+            name (str): Name of the workflow to be created
+            dataset_id (int, optional): Dataset ID for the newly created workflow
+                uses self.dataset.id if no dataset_id provided.
+
+        Raises:
+            ToolkitInputError: If no dataset_id provided and one not found durring class instantiation.
+        """
         if not self.dataset and not dataset_id:
             raise ToolkitInputError(
                 "This call requires a dataset object when instatiating Structure or a provided dataset_id"
             )
+        if dataset_id and not self.dataset:
+            self.dataset = self.client.call(GetDataset(dataset_id))
+            print(f"Updating class dataset to id: {dataset_id}")
 
-        if not dataset_id:
+        if not dataset_id and self.dataset:
             dataset_id = self.dataset.id
 
         self.workflow = self.client.call(
@@ -195,57 +201,99 @@ class Structure:
         self.workflow_id = self.workflow.id
         self.ocr_id = self.workflow.component_by_type("INPUT_OCR_EXTRACTION").id
         sleep(2)
+        return self.workflow
 
     def add_teach_task(
         self,
         task_name: str,
         labelset_name: str,
         target_names: List[str],
-        model_type=ModelTaskType.ANNOTATION,
+        model_type="annotation",
         previous_component_id: int = None,
         data_column: str = "document",
         dataset_id: int = None,
         workflow_id: int = None,
+        **kwargs,
     ) -> None:
         """
         Args:
-            task_name (str): _description_
-            labelset_name (str): _description_
-            target_names (List[str]): _description_
-            model_type (_type_, optional): _description_. Defaults to ModelTaskType.ANNOTATION.
+            task_name (str): Teach task name
+            labelset_name (str): Name for created labelset
+            target_names (List[str]): List of target (label) names
+            model_type (_type_, optional): Defaults to annotation.
+                Available model types:
+                    classification
+                    form_extraction
+                    object_detection
+                    classification_multiple
+                    regression
+                    annotation
+                    classification_unbundling
             previous_component_id (int, optional): _description_. Defaults to None.
-            data_column (str, optional): _description_. Defaults to "document".
-            dataset_id (int, optional): _description_. Defaults to None.
-            workflow_id (int, optional): _description_. Defaults to None.
+            data_column (str, optional): Defaults to "document".
+            dataset_id (int, optional): Dataset ID. Will use self.dataset.id if not provided
+            workflow_id (int, optional): Workflow ID. Will use self.workflow.id if not provided.
 
         Raises:
-            Exception: _description_
+            ToolkitInputError: If using an incorrect model type. Please view correct model types for options.
+            ToolkitInputError: Needs to have a dataset, workflow, and either previous component ID or ocr ID.
+
+        TODO: clean up kwargs to advanced model training options
         """
+        if model_type not in model_map.keys():
+            raise ToolkitInputError(
+                f"{model_type} not found. Available options include {[model for model in model_map.keys()]}"
+            )
+
+        model_map = {
+            "classification": ModelTaskType.CLASSIFICATION,
+            "form_extraction": ModelTaskType.FORM_EXTRACTION,
+            "object_detection": ModelTaskType.OBJECT_DETECTION,
+            "classification_multiple": ModelTaskType.CLASSIFICATION_MULTIPLE,
+            "regression": ModelTaskType.REGRESSION,
+            "annotation": ModelTaskType.ANNOTATION,
+            "classification_unbundling": ModelTaskType.CLASSIFICATION_UNBUNDLING,
+        }
         prev_comp_id = previous_component_id if previous_component_id else self.ocr_id
         if (
             (not self.dataset and not dataset_id)
             or not prev_comp_id
             or (not self.workflow and not workflow_id)
         ):
-            raise Exception(
+            raise ToolkitInputError(
                 "Must have dataset, workflow and** either previous component ID or ocr ID"
             )
+
+        if dataset_id:
+            self.dataset = (
+                self.client.call(GetDataset(dataset_id)) if dataset_id else self.dataset
+            )
+            print(f"Updating current self.dataset with dataset id: {dataset_id}")
+        if workflow_id:
+            self.workflow = (
+                self.client.call(GetWorkflow(workflow_id))
+                if workflow_id
+                else self.workflow
+            )
+            print(f"Updating current self.workflow with workflow id: {workflow_id}")
+
         column_id = self.dataset.datacolumn_by_name(data_column).id
         new_labelset = NewLabelsetArguments(
             labelset_name,
             datacolumn_id=column_id,
-            task_type=model_type,
+            task_type=model_map[model_type],
             target_names=target_names,
         )
 
         self.workflow = self.client.call(
             AddModelGroupComponent(
-                workflow_id=workflow_id,
+                workflow_id=self.workflow.id,
                 name=task_name,
-                dataset_id=dataset_id,
+                dataset_id=self.dataset.id,
                 source_column_id=column_id,
                 after_component_id=prev_comp_id,
                 new_labelset_args=new_labelset,
+                model_training_options=kwargs,
             )
         )
         self.last_component_id = self.workflow.components[-1].id
@@ -254,12 +302,12 @@ class Structure:
         ).model_group.questionnaire_id
         print(f"Component Added with Teach ID:{self.last_questionnaire_id} ")
         sleep(3)
-    
+
     def add_class_filter(
         self, classes_to_filter: List[str], classification_task_name: str
     ):
         if not self.last_component_id or not self.workflow:
-            raise Exception(
+            raise ToolkitInputError(
                 "Need to have a last component ID for classification model and workflow obj"
             )
         filtered = AddLinkClassificationComponent(
