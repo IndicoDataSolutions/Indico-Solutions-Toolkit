1f01d2202e3f2d076c49581b748cfcedc3fbbc18
Michael Welborn <michael.welborn@indicodata.ai>
2023-09-22 15:27:05 -0500

    Add utilities for working with result file objects

---
+++ indico_toolkit/types/utils.py
@@ -0,0 +1,30 @@
+from typing import TypeVar
+
+from .errors import ResultFileError
+
+Value = TypeVar("Value")
+
+
+def exists(result: object, key: str, value_type: type[Value]) -> bool:
+    """
+    Check if `key` exists in `result` having type `value_type`.
+    """
+    return (
+        isinstance(result, dict)
+        and key in result
+        and isinstance(result[key], value_type)
+    )
+
+
+def get(result: object, key: str, value_type: type[Value]) -> Value:
+    """
+    Return the value of `key` from `result` if `result` is a dict and the value has type
+    `value_type`. Raise a `ResultFileError` otherwise.
+    """
+    if exists(result, key, value_type):
+        return result[key]  # type: ignore[index, no-any-return]
+    else:
+        raise ResultFileError(
+            f"Result object `{type(result)!r}` does not have a value for "
+            f"key `{key!r}` with type `{value_type}`."
+        )
