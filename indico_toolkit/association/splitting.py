fd3c3109ef5943eed2b5a85b7ecf8928e3098713
Michael Welborn <michael.welborn@indicodata.ai>
2023-12-13 15:23:50 -0600

    Improve the docstring and explain the text/delimiter pairing logic

--- indico_toolkit/association/splitting.py
+++ indico_toolkit/association/splitting.py
@@ -9,8 +9,18 @@ def split_prediction(
 ) -> Iterator[Dict[str, Any]]:
     """
     Split a single `prediction` into mutiple predictions along a `separator` regex
-    and yield new predictions having updated text and spans.
+    and yield new predictions having updated text and spans. By default, split on
+    newlines (including adjacent whitespace).
     """
+
+    # re.split (with group capture) returns a sequence of text values and delimiters:
+    # ["some", "\n", "separated", "\n", "text"]
+    # The text values are the even elements:
+    # ["some", "separated", "text"]
+    # The delimeters are the odd elements:
+    # ["\n", "\n"]
+    # zip_longest pairs them together without losing the last text value:
+    # [("some", "\n"), ("separated", "\n"), ("text", None)]
     texts_and_delimiters = re.split(f"({separator})", prediction["text"])
     texts = texts_and_delimiters[::2]
     delimiters = texts_and_delimiters[1::2]
